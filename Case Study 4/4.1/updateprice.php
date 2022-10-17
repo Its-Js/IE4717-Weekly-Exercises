cb5346609e6e3499c9013069cc7263f04883ea58
Its-Js <jiangshan0098@gmail.com>
2022-10-17 23:49:26 +0800

    update

---
+++ Case Study 4/4.1/updateprice.php
@@ -0,0 +1,11 @@
+<?php
+$jjpricesingle = $_POST['jj-price-single'];
+$calpricesingle = $_POST['cal-price-single'];
+$calpricedouble = $_POST['cal-price-double'];
+$icpricesingle = $_POST['ic-price-single'];
+$icpricedouble = $_POST['ic-price-double'];
+?>
+
+<html>
+
+</html>
